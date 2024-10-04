3dbd45664a7e7d9a2d1a8336f98bb1b3c7010919
vebrian 2019 <vebrian999@gmail.com>
2024-10-04 13:41:25 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#682E74",
+        "secondary-color": "#5BC7D4",
+        "third-color": "#FFEAFD",
+      },
+    },
+  },
+  plugins: [],
+};
