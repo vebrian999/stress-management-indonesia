5e2820df93d195ee80ae3d6b8c8b802c9aaf2072
vebrian 2019 <vebrian999@gmail.com>
2024-10-05 21:35:10 +0700

    fix navbar

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,8 @@
 window.addEventListener("scroll", function () {
-  const header = document.getElementById("header");
+  const navbar = document.getElementById("navbar");
   if (window.scrollY > 50) {
-    header.classList.add("scrolled");
+    navbar.classList.add("scrolled");
   } else {
-    header.classList.remove("scrolled");
+    navbar.classList.remove("scrolled");
   }
 });
